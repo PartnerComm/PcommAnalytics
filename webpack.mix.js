78a2ac9c78a8d7dee4ca16549d43803961d37dd1
tbunch <timbunch@gmail.com>
2019-04-18 09:57:12 -0700

    make analytics fully accessible in the window object

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,3 @@
 let mix = require('laravel-mix');
-mix
-  .js('lib/analytics.js', 'dist')
-  .babel('dist/analytics.js', 'dist/analytics-babel.js');
+mix.js('lib/analytics.js', 'dist');
+//.babel('dist/analytics.js', 'dist/analytics-babel.js')
